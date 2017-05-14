1a21a3e06a33979c5ed43ca330c3b3b52d34323d
rapier83 <rapier83@gmail.com>
2017-05-14 10:46:08 +0900

    change searchBar style by pretty way, I think.

--- mapBaseApp/AppDelegate.swift
+++ mapBaseApp/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAf4UUmBE9ttb9GsgeP65eezfBFJqZJN1U")
+        GMSPlacesClient.provideAPIKey("AIzaSyAf4UUmBE9ttb9GsgeP65eezfBFJqZJN1U")
         return true
     }
 
--- mapBaseApp/ViewController.swift
+++ mapBaseApp/ViewController.swift
@@ -7,11 +7,42 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var TopSearchBar: UISearchBar! {
+        didSet {
+            TopSearchBar?.clipsToBounds = true
+            TopSearchBar?.placeholder = "좋은 검색어를 넣어보세요."
+            TopSearchBar?.setImage(UIImage(named: "whiteArrow"), for: UISearchBarIcon.search, state: UIControlState.normal)
+            TopSearchBar?.layer.borderWidth = 0
+            TopSearchBar?.layer.cornerRadius = 0
+            TopSearchBar?.layer.shadowOpacity = 0.8
+            TopSearchBar?.layer.shadowOffset = CGSize(width: 2, height: 2)
+            TopSearchBar?.tintColor = UIColor.clear
+
+        }
+    }
+    
+
+    class searchBar: UISearchBar, UISearchBarDelegate {
+        override var placeholder: String? {
+            didSet {
+                if let text = placeholder {
+                    if text.substring(from: text.endIndex) != " " {
+                        
+                    }
+                }
+            }
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(self.view)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
