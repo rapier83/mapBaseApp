0d9e522c0ae24b8271a6aac5cc8ecda4d0ec2a67
Junmo Kang <rapier83@gmail.com>
2017-08-02 15:32:26 +0900

    RE: Initial Commits. To do Best!

--- mapBaseApp/ViewController.swift
+++ mapBaseApp/ViewController.swift
@@ -11,34 +11,26 @@ import GoogleMaps
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var TopSearchBar: UISearchBar! {
+    
+    @IBOutlet weak var SearchBar: UISearchBar! {
         didSet {
-            TopSearchBar?.clipsToBounds = true
-            TopSearchBar?.placeholder = "좋은 검색어를 넣어보세요."
-            TopSearchBar?.setImage(UIImage(named: "whiteArrow"), for: UISearchBarIcon.search, state: UIControlState.normal)
-            TopSearchBar?.layer.borderWidth = 0
-            TopSearchBar?.layer.cornerRadius = 0
-            TopSearchBar?.layer.shadowOpacity = 0.8
-            TopSearchBar?.layer.shadowOffset = CGSize(width: 2, height: 2)
-            TopSearchBar?.tintColor = UIColor.clear
-
+            SearchBar?.clipsToBounds = true
+            SearchBar?.placeholder = "좋은 검색어를 넣어보세요."
+            SearchBar?.layer.borderWidth = 0
+            SearchBar?.layer.cornerRadius = 0
+            SearchBar?.layer.shadowOpacity = 0.8
+            SearchBar?.layer.shadowOffset = CGSize(width: 2, height: 2)
+            SearchBar?.tintColor = UIColor.clear
+            
         }
     }
     
 
-    class searchBar: UISearchBar, UISearchBarDelegate {
-        override var placeholder: String? {
-            didSet {
-                if let text = placeholder {
-                    if text.substring(from: text.endIndex) != " " {
-                        
-                    }
-                }
-            }
-        }
-    }
+
     
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
